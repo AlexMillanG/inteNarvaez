4bed071eeabe71f5e9d6388635a1ab55c384eb95
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2025-03-02 17:58:05 -0600

    primer commit

---
+++ src/main/java/mx/edu/utez/inteNarvaez/InteNarvaezApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.inteNarvaez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteNarvaezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteNarvaezApplication.class, args);
+	}
+
+}
