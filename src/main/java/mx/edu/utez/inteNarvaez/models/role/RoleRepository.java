2c7e00223c2d61832896ce9e24de966d2830ba29
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2025-03-22 20:13:32 -0600

    Update RoleRepository.java

--- src/main/java/mx/edu/utez/inteNarvaez/models/role/RoleRepository.java
+++ src/main/java/mx/edu/utez/inteNarvaez/models/role/RoleRepository.java
@@ -3,6 +3,10 @@ package mx.edu.utez.inteNarvaez.models.role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<RoleBean,Long> {
+
+    Optional<RoleBean> findByName(String s);
 }
