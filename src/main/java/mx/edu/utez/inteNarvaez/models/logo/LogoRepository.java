f5f30835b627d657b5a6c069f21f8e4042f0bd20
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2025-04-10 14:50:51 -0600

    update de canal con imagen

--- src/main/java/mx/edu/utez/inteNarvaez/models/logo/LogoRepository.java
+++ src/main/java/mx/edu/utez/inteNarvaez/models/logo/LogoRepository.java
@@ -3,6 +3,10 @@ package mx.edu.utez.inteNarvaez.models.logo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LogoRepository extends JpaRepository<LogoBean,Long> {
+
+    Optional<LogoBean> findByChannel_Id(Long id);
 }
