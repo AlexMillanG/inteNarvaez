1f50ef50b27be02799e2c4809a672159aaf10c90
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2025-04-12 16:24:07 -0600

    usuario que no tiene rol

--- src/main/java/mx/edu/utez/inteNarvaez/config/InitialConfig.java
+++ src/main/java/mx/edu/utez/inteNarvaez/config/InitialConfig.java
@@ -7,27 +7,38 @@ import mx.edu.utez.inteNarvaez.models.channelCategory.ChannelCategoryBean;
 import mx.edu.utez.inteNarvaez.models.channelCategory.ChannelCategoryRepository;
 import mx.edu.utez.inteNarvaez.models.role.RoleBean;
 import mx.edu.utez.inteNarvaez.models.role.RoleRepository;
+import mx.edu.utez.inteNarvaez.models.user.UserEntity;
+import mx.edu.utez.inteNarvaez.models.user.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @AllArgsConstructor
 @Component
 public class InitialConfig {
 
     private final ChannelCategoryRepository channelCategoryRepository;
-
+    private final PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
     private final ChannelRepository channelRepository;
     private final RoleRepository roleRepository;
 
 
     @Bean
+    @Transactional
     public CommandLineRunner initData() {
         return args -> {
+
+            roles(new RoleBean("ADMIN", UUID.randomUUID()));
+            roles(new RoleBean("USER", UUID.randomUUID()));
+
+
+            createUser("admin","admin","admin","ASDFGHJKLASDF","admin1@admin.com","1234567890","1234");
 
             createCategoryChannel("Infantil");
             createCategoryChannel("Deportes");
@@ -37,8 +48,6 @@ public class InitialConfig {
             createChannel("ESPN","ahí pasan futbol picante", 234,"https://static.wikia.nocookie.net/telepedia-es/images/9/99/ESPN_logo.png/revision/latest?cb=20210826204726&path-prefix=es",2L);
             createChannel("FOX", "nose, ya no lo veo",45435,"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Fox_Channel_logo.svg/2560px-Fox_Channel_logo.svg.png",3L);
 
-            roles(new RoleBean("ADMIN", UUID.randomUUID()));
-            roles(new RoleBean("USER", UUID.randomUUID()));
 
         };
     }
@@ -69,9 +78,6 @@ public class InitialConfig {
         }
     }
 
-
-
-
     private void createChannel(String name, String description, Integer number, String image, Long categoryId) {
         Optional<ChannelBean> foundChannel = channelRepository.findByName(name);
         if (foundChannel.isEmpty()) {
@@ -89,6 +95,30 @@ public class InitialConfig {
             } else {
                 System.err.println("Error: La categoría con ID " + categoryId + " no existe");
             }
+        }
+    }
+
+    private void createUser(String name, String lastname, String surname, String rfc, String email, String phone,String password){
+       Optional<UserEntity> foundAdmin = userRepository.findByEmail(email);
+        if (foundAdmin.isEmpty()){
+            UserEntity user = new UserEntity();
+            user.setFirstName(name);
+            user.setLastName(lastname);
+            user.setSurname(surname);
+            user.setRfc(rfc);
+            user.setEmail(email);
+            user.setPhone(phone);
+            user.setBirthdate(new Date());
+            user.setLastLogin(new Date());
+            user.setStatus(true);
+            user.setPassword(passwordEncoder.encode(password));
+            user.setTemporalPassword(false);
+
+
+            userRepository.save(user);
+            System.err.println("Usuario " + name + " creado");
+        } else {
+            System.err.println("El usuario ya existe");
         }
     }
 
