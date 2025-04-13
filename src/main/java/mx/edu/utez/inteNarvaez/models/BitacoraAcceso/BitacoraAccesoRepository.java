253a60a42e7fa2e7fcac97fd6bc34321b17381a2
MartinOM321753 <133397788+MartinOM321753@users.noreply.github.com>
2025-04-13 00:28:04 -0600

    Se añadio funcionalidad de registro de acceso al proceso de autenticación y crear entidad BitacoraAcceso

---
+++ src/main/java/mx/edu/utez/inteNarvaez/models/BitacoraAcceso/BitacoraAccesoRepository.java
@@ -0,0 +1,7 @@
+package mx.edu.utez.inteNarvaez.models.BitacoraAcceso;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BitacoraAccesoRepository extends JpaRepository<BitacoraAccesoEntity, Long> {
+
+}
