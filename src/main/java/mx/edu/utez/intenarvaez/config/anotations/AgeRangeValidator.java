bfe07c4e51d5d72a9011fb01723ebd7f6715de80
MartinOM321753 <133397788+MartinOM321753@users.noreply.github.com>
2025-04-20 10:20:55 -0600

    Cambio en tipo de dato

--- src/main/java/mx/edu/utez/intenarvaez/config/anotations/AgeRangeValidator.java
+++ src/main/java/mx/edu/utez/intenarvaez/config/anotations/AgeRangeValidator.java
@@ -1,21 +1,24 @@
 package mx.edu.utez.intenarvaez.config.anotations;
-
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
 
-public class AgeRangeValidator implements ConstraintValidator<ValidAgeRange, LocalDate> {
+public class AgeRangeValidator implements ConstraintValidator<ValidAgeRange, Date> {
 
     private static final int MIN_AGE = 18;
     private static final int MAX_AGE = 100;
 
     @Override
-    public boolean isValid(LocalDate date, ConstraintValidatorContext context) {
+    public boolean isValid(Date date, ConstraintValidatorContext context) {
         if (date == null) return false;
 
-        int age = Period.between(date, LocalDate.now()).getYears();
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        int age = Period.between(localDate, LocalDate.now()).getYears();
         return age >= MIN_AGE && age <= MAX_AGE;
     }
 }
+
