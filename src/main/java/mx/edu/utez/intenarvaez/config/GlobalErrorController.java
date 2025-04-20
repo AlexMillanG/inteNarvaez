8fe45e09c32a09703bc12448bf64769445809b2d
MartinOM321753 <133397788+MartinOM321753@users.noreply.github.com>
2025-04-19 20:46:39 -0600

    Se agrego registro para el manejo de errores en GlobalErrorController

--- src/main/java/mx/edu/utez/intenarvaez/config/GlobalErrorController.java
+++ src/main/java/mx/edu/utez/intenarvaez/config/GlobalErrorController.java
@@ -1,4 +1,6 @@
 package mx.edu.utez.intenarvaez.config;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -17,10 +19,13 @@ import java.sql.SQLException;
 @RestControllerAdvice
 public class GlobalErrorController {
 
+    private static final Logger logger = LogManager.getLogger(GlobalErrorController.class);
 
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ApiResponse> handleNotFound404(NoHandlerFoundException ex) {
+        logger.error("Error 404: {}", ex.getMessage());
+
         return new ResponseEntity<>(
                 new ApiResponse(null, HttpStatus.NOT_FOUND, "¡Ups! El recurso no se encuentra disponible, intenta más tarde", true),
                 HttpStatus.NOT_FOUND
@@ -30,6 +35,7 @@ public class GlobalErrorController {
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ApiResponse> handleBadRequest400(MethodArgumentTypeMismatchException ex) {
+        logger.error("Error 400: {}", ex.getMessage());
         return new ResponseEntity<>(
                 new ApiResponse(null, HttpStatus.BAD_REQUEST, "¡Error! Hiciste mal la solicitud", true),
                 HttpStatus.BAD_REQUEST
@@ -39,6 +45,7 @@ public class GlobalErrorController {
     @ExceptionHandler(NoResourceFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ApiResponse> handleNoResourceFoundException(NoResourceFoundException ex, Model model) {
+        logger.error("Error 404 : La URL no existe {}", ex.getMessage());
         model.addAttribute("mensaje", "La URL no existe");
         return new ResponseEntity<>(
                 new ApiResponse(null, HttpStatus.NOT_FOUND, "La URL no existe", true),
@@ -49,6 +56,7 @@ public class GlobalErrorController {
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     public ResponseEntity<ApiResponse> handleNotAcceptableException(HttpMediaTypeNotAcceptableException ex) {
+        logger.error("Error 406: {}", ex.getMessage());
         return new ResponseEntity<>(
                 new ApiResponse(null, HttpStatus.NOT_ACCEPTABLE, "Formato de respuesta no soportado", true),
                 HttpStatus.NOT_ACCEPTABLE
@@ -58,6 +66,7 @@ public class GlobalErrorController {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ApiResponse> handleGenericException500(Exception ex) {
+        logger.error("Error 500: {}", ex.getMessage());
         return new ResponseEntity<>(
                 new ApiResponse(null, HttpStatus.INTERNAL_SERVER_ERROR, "¡Ups! Algo salió mal, pero no te preocupes, estamos en ello", true),
                 HttpStatus.INTERNAL_SERVER_ERROR
@@ -67,6 +76,7 @@ public class GlobalErrorController {
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ResponseEntity<ApiResponse> handleForbidden403(AccessDeniedException ex) {
+        logger.error("Error 403: {}", ex.getMessage());
         return new ResponseEntity<>(
                 new ApiResponse(null, HttpStatus.FORBIDDEN, "Acceso denegado", true),
                 HttpStatus.FORBIDDEN
@@ -76,6 +86,7 @@ public class GlobalErrorController {
     @ExceptionHandler(IllegalAccessException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ResponseEntity<ApiResponse> handleUnauthorized401(IllegalAccessException ex) {
+        logger.error("Error 401: {}", ex.getMessage());
         return new ResponseEntity<>(
                 new ApiResponse(null, HttpStatus.UNAUTHORIZED, "No tienes la autorización para continuar", true),
                 HttpStatus.UNAUTHORIZED
@@ -85,6 +96,7 @@ public class GlobalErrorController {
     @ExceptionHandler(SQLException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ApiResponse> handleDatabaseException500(SQLException ex) {
+        logger.error("Error 500 : No podemos conectarnos a la base de datos {}", ex.getMessage());
         return new ResponseEntity<>(
                 new ApiResponse(null, HttpStatus.INTERNAL_SERVER_ERROR, "No podemos conectarnos a la base de datos", true),
                 HttpStatus.INTERNAL_SERVER_ERROR
